7eba3f67b5c0b512c147b30c9a6b8cfd85c3cd51
Daniel Morales <contactardanielmorales@gmail.com>
2023-02-15 18:25:13 -0500

    remove _site/

--- _site/assets/js/search-data.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "My First Post",
-        "excerpt":"Ampère’s circuital law (with Maxwell’s addition) \\[\\oint_{a x} \\mathbf{B} \\cdot d \\ell=\\mu_0\\left(\\iint_z \\mathrm{~J} \\cdot \\mathrm{d} z+\\varepsilon_0 \\frac{\\mathrm{d}}{d t} \\iint_z \\mathbf{E} \\cdot \\mathrm{dS}\\right)\\]","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/my-first-post/"
-      }]
